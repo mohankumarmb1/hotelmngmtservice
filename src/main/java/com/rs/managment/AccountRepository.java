f1370ae669c6718a961dd53194129e2c909a5017
Mohan Kumar M B <apple@Apples-Air.Dlink>
2026-02-18 23:42:34 +0530

    code changes commit

--- src/main/java/com/rs/managment/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rs.managment;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AccountRepository extends MongoRepository<Account, String> {
-
-
-}
---
+++ src/test/java/com/hrs/hotelmanagementservice/HotelManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrs.hotelmanagementservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HotelManagementServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
